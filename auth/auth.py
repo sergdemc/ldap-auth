dc4ffd34631fa4ac58bdd316af2da7185ee80fe3
Sergei Demchenko <hb13718@gmail.com>
2023-11-02 14:06:49 +0300

    Init

---
+++ auth/auth.py
@@ -0,0 +1,55 @@
+import ldap
+
+
+class LDAPAuth:
+    def __init__(self, ldap_host, ldap_port, ldap_base_dn, ldap_admin, ldap_admin_password, secret_key):
+        self.ldap_host = ldap_host
+        self.ldap_port = ldap_port
+        self.ldap_base_dn = ldap_base_dn
+        self.ldap_admin = ldap_admin
+        self.ldap_admin_password = ldap_admin_password
+        self.ldap_connection = self.connect()
+
+        self.secret_key = secret_key
+
+    def connect(self):
+        ldap_connection = ldap.initialize('ldap://' + self.ldap_host + ':' + str(self.ldap_port))
+        ldap_connection.simple_bind_s(self.ldap_admin, self.ldap_admin_password)
+        return ldap_connection
+
+    def disconnect(self):
+        self.ldap_connection.unbind()
+
+    def add_user(self, username: str, password: str, user_id: str):
+        try:
+            self.ldap_connection.search_s(self.ldap_base_dn, ldap.SCOPE_SUBTREE, '(uid=' + user_id + ')')
+            return (False, f'User ID: {user_id} already exists')
+        except ldap.NO_SUCH_OBJECT:
+            pass
+        except ldap.SERVER_DOWN:
+            return (False, 'LDAP server is down')
+        dn = 'cn=' + username + ',' + self.ldap_base_dn
+        attrs = [
+            ('objectClass', [b'top', b'person', b'organizationalPerson', b'inetOrgPerson']),
+            ('cn', username.encode('utf-8')),
+            ('sn', username.encode('utf-8')),
+            ('userPassword', password.encode('utf-8')),
+            ('uid', user_id.encode('utf-8'))
+        ]
+        try:
+            self.ldap_connection.add_s(dn, attrs)
+            return True
+        except ldap.LDAPError as e:
+            return False, e
+
+    def authenticate(self, username, password) -> bool:
+        try:
+            user_dn = 'cn=' + username + ',' + self.ldap_base_dn
+            self.ldap_connection.simple_bind_s(user_dn, password)
+            return True
+        except ldap.INVALID_CREDENTIALS:
+            return False
+        except ldap.SERVER_DOWN:
+            return False
+        except ldap.LDAPError:
+            return False
