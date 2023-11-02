a610189234dbf208e05c58e4326faa99e574ee92
Sergei Demchenko <hb13718@gmail.com>
2023-11-02 15:25:40 +0300

    Add password hashing and JWT

--- auth/auth.py
+++ auth/auth.py
@@ -1,3 +1,5 @@
+import hashlib
+import jwt
 import ldap
 
 
@@ -20,6 +22,24 @@ class LDAPAuth:
     def disconnect(self):
         self.ldap_connection.unbind()
 
+    @staticmethod
+    def hash_password(password) -> str:
+        algorithm = hashlib.sha256()
+        password_bytes = password.encode('utf-8')
+        algorithm.update(password_bytes)
+        password_hashed = algorithm.hexdigest()
+        return password_hashed
+
+    def generate_access_token(self, user_id: str) -> str:
+        return jwt.encode({"user_id": user_id}, self.secret_key, algorithm="HS256")
+
+    def validate_access_token(self, token: str) -> str | None:
+        try:
+            payload = jwt.decode(token, self.secret_key, algorithms="HS256")
+            return payload
+        except:
+            return None
+
     def add_user(self, username: str, password: str, user_id: str):
         try:
             self.ldap_connection.search_s(self.ldap_base_dn, ldap.SCOPE_SUBTREE, '(uid=' + user_id + ')')
@@ -28,12 +48,13 @@ class LDAPAuth:
             pass
         except ldap.SERVER_DOWN:
             return (False, 'LDAP server is down')
+
         dn = 'cn=' + username + ',' + self.ldap_base_dn
         attrs = [
             ('objectClass', [b'top', b'person', b'organizationalPerson', b'inetOrgPerson']),
             ('cn', username.encode('utf-8')),
             ('sn', username.encode('utf-8')),
-            ('userPassword', password.encode('utf-8')),
+            ('userPassword', self.hash_password(password).encode('utf-8')),
             ('uid', user_id.encode('utf-8'))
         ]
         try:
@@ -47,7 +68,7 @@ class LDAPAuth:
     def authenticate(self, username, password):
         try:
             user_dn = 'cn=' + username + ',' + self.ldap_base_dn
-            self.ldap_connection.simple_bind_s(user_dn, password)
+            self.ldap_connection.simple_bind_s(user_dn, self.hash_password(password))
             return True
         except ldap.INVALID_CREDENTIALS:
             return (False, 'Invalid credentials')
